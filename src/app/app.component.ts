e5eaed4604a461c29b34713537b3d85d6d075a3e
Hariprasaththangaraj <hariprasaththangaraj@gmail.com>
2023-08-26 23:41:26 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,56 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'angular-simple-crud';
+  initialValue = '';
+  newData: string[] = [];
+  completedData: string[] = [];
+  completeDataItem: string[] = [];
+  indexData: number | null = null;
+
+
+  save() {
+    if (this.indexData != null) {
+      this.newData = this.newData.map((val, i) => {
+        if (this.indexData == i) {
+          val = this.initialValue;
+        }
+        return val;
+      })
+    } else {
+      this.newData.push(this.initialValue);
+    }
+    this.indexData = null;
+    this.initialValue = '';
+  }
+
+  update(indexData: number) {
+    this.indexData = indexData;
+    const editData = this.newData.find((val, i) => {
+      return indexData == i;
+    });
+    if (editData)
+      this.initialValue = editData;
+  }
+
+  delete(indexData: number) {
+    this.newData = this.newData.filter((val, i) => {
+      return indexData !== i;
+    }
+    )
+  }
+
+  complete(completeData: number) {
+    const completeDataItem = this.newData.find((val, i) => {
+      return completeData == i;
+    })
+    this.newData = this.newData.filter((val, i) => {
+      return completeData !== i;
+    })
+    if (completeDataItem)
+      this.completeDataItem.push(completeDataItem)
+    console.log(this.completeDataItem)
+  }
 }
